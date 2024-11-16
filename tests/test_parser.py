e592064214b71dad74ac21f42b41aa553f766ba8
n0tphotogenic <tbw4530@gmail.com>
2024-11-16 02:12:42 +0000

    test(parser): modified test to add description key

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -4,21 +4,28 @@ from src.parser import CraftParser
 class TestCraftParser(unittest.TestCase):
   @classmethod
   def setUpClass(cls):
-    cls.craft = "Craft: Witching Tisane\n"
-    "Hush House still rings with thwarted desires. I'll mix that into a tisane.\n"
-    "[Desires & Dissolutions with Prentice-level <sprite name=Grail> creates "
-    "Witching Tisane.]"
+    cls.craft = (
+      "Craft: Witching Tisane\n"
+      "Hush House still rings with thwarted desires. I'll mix that into a tisane.\n"
+      "[Desires & Dissolutions with Prentice-level <sprite name=Grail> creates" 
+      "Witching Tisane.]"
+    )
     cls.craft_parser = CraftParser()
     cls.parsed_data = cls.craft_parser.parse(cls.craft)
 
   def test_parsed_data_structure(self):
     self.assertIsInstance(self.parsed_data, dict)
     self.assertSetEqual(
-        set(self.parsed_data.keys()), {"result", "skill", "level", "soul"}
+        set(self.parsed_data.keys()), {"result", "description", "skill", 
+                                       "level", "soul"}
     )
 
   def test_parse_crafting_result(self):
     self.assertEqual(self.parsed_data["result"], "Witching Tisane")
+
+  def test_parse_crafting_description(self):
+    self.assertEqual(self.parsed_data["description"], ("Hush House still rings " 
+                     "with thwarted desires. I'll mix that into a tisane."))
 
   def test_parse_crafting_skill(self):
     self.assertEqual(self.parsed_data["skill"], "Desires & Dissolutions")
